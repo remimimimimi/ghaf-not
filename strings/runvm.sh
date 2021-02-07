0804ee7e97426714a048e56ed3068b10662447c4
Vo Minh Thu <thu@hypered.io>
2021-02-07 15:38:12 +0100

    Re-add the 80 and 443 port forwarding.

--- strings/runvm.sh
+++ strings/runvm.sh
@@ -11,10 +11,7 @@ exec ${pkgs.qemu_kvm}/bin/qemu-kvm \
   -drive index=0,id=drive1,file=${config.system.build.squashfs},readonly,media=cdrom,format=raw,if=virtio \
   -append "console=ttyS0 ${toString config.boot.kernelParams} quiet panic=-1" \
   -device virtio-net,netdev=net0 \
-  -netdev user,id=net0,net=10.0.2.0/24,host=10.0.2.2,dns=10.0.2.3,hostfwd=tcp::2222-:22 \
+  -netdev user,id=net0,net=10.0.2.0/24,host=10.0.2.2,dns=10.0.2.3,hostfwd=tcp::2222-:22,hostfwd=tcp::9080-:80,hostfwd=tcp::9443-:443 \
   -device virtio-rng-pci \
   -nographic \
   -no-reboot
-
-# TODO Doesn't seem supported anymore, must be added back in some way.
-#  -redir tcp:8000::80 \
